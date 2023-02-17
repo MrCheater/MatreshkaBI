77b11a0586e4b9a5e8e12d4bb15aec15fae8dab6
Anzhelika <mrstonem@mail.ru>
2023-02-17 23:26:44 +0300

    filters

--- components/FilterPanel/FilterPanel.jsx
+++ components/FilterPanel/FilterPanel.jsx
@@ -1,8 +1,24 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Select, Stack, FormControl, MenuItem, InputLabel} from '@mui/material';
 import PropTypes from 'prop-types';
+import monthsJSON from "../../data/months.json";
+import quartersJSON from "../../data/quarters.json";
+import regionsJSON from "../../data/regions.json";
+import yearsJSON from "../../data/years.json";
+
 
 export const FilterPanel = () => {
+  const [months, setMonths] = useState(monthsJSON);
+  const [month, setMonth] = useState(months[0] ?? '');
+
+  const [quarters, setQuarters] = useState(quartersJSON);
+  const [quarter, setQuarter] = useState(quarters[0] ?? '');
+
+  const [regions, setRegions] = useState(regionsJSON);
+  const [region, setRegion] = useState(regions[0] ?? '');
+
+  const [years, setYears] = useState(yearsJSON);
+  const [year, setYear] = useState(years[0] ?? '');
 
   return (
     <Stack
@@ -16,16 +32,13 @@ export const FilterPanel = () => {
         <Select
           labelId="demo-simple-select-standard-label"
           id="demo-simple-select-standard"
-          // value={age}
-          // onChange={handleChange}
-          label="год"
+          value={year}
+          onChange={() => setYear(year)}
         >
           <MenuItem value="">
             <em>None</em>
           </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+          {years?.map((year) => <MenuItem value={year}>{year}</MenuItem>)}
         </Select>
       </FormControl>
       <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
@@ -33,50 +46,44 @@ export const FilterPanel = () => {
         <Select
           labelId="demo-simple-select-standard-label"
           id="demo-simple-select-standard"
-          // value={age}
-          // onChange={handleChange}
+          value={month}
+          onChange={() => setMonth(month)}
           label="месяц"
         >
           <MenuItem value="">
             <em>None</em>
           </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+          {months?.map((month) => <MenuItem value={month}>{month}</MenuItem>)}
         </Select>
       </FormControl>
       <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-simple-select-standard-label">регион</InputLabel>
+        <InputLabel id="demo-simple-select-standard-label">квартал</InputLabel>
         <Select
           labelId="demo-simple-select-standard-label"
           id="demo-simple-select-standard"
-          // value={age}
-          // onChange={handleChange}
-          label="регион"
+          value={quarter}
+          onChange={() => setQuarter(quarter)}
+          label="квартал"
         >
           <MenuItem value="">
             <em>None</em>
           </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+          {quarters?.map((quarter) => <MenuItem value={quarter}>{quarter}</MenuItem>)}
         </Select>
       </FormControl>
       <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-simple-select-standard-label">муниципальное образование</InputLabel>
+        <InputLabel id="demo-simple-select-standard-label">регионы</InputLabel>
         <Select
           labelId="demo-simple-select-standard-label"
           id="demo-simple-select-standard"
-          // value={age}
-          // onChange={handleChange}
-          label="муниципальное образование"
+          value={region}
+          onChange={() => setRegion(region)}
+          label="регионы"
         >
           <MenuItem value="">
             <em>None</em>
           </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+          {regions?.map((region) => <MenuItem value={region}>{region}</MenuItem>)}
         </Select>
       </FormControl>
     </Stack>
